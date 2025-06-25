13ee737e3f74389f5960c1f8a661f47a066f920f
chase784 <walfall784@gmail.com>
2025-06-25 12:52:37 -0500

    Computer scientists still failing to clone a repository. Doomed!

---
+++ apl-vue/src/components/OutputPanel.vue
@@ -0,0 +1,25 @@
+<!-- src/components/OutputPanel.vue -->
+<template>
+    <div>
+        <h2 class="text-xl">Output</h2>
+        <pre v-if="output">{{ output }}</pre>
+        <pre v-if="error" class="error">{{ error }}</pre>
+        <div v-if="llmExplanation">
+            <h3>LLM Explanation</h3>
+            <p>{{ llmExplanation }}</p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'OutputPanel',
+    props: {
+        output: { type: String, default: '' },
+        error: { type: String, default: '' },
+        llmExplanation: { type: String, default: '' }
+    }
+})
+</script>
